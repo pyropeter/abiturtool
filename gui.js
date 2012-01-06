e31d5e98ed9b5188071bb66f779c1452f89f3c9d
PyroPeter <abi1789@googlemail.com>
2012-01-07 00:28:20 +0100

    load/saveNotenliste

--- gui.js
+++ gui.js
@@ -23,7 +23,40 @@ function initNotentabelle() {
 	}
 }
 
+function loadNotentabelle() {
+	$("#noten input").each(function () {
+		var input = $(this);
+		var note = noten[input.data("halbjahr")][input.data("fach")];
+		if (typeof note == "number")
+			input.val(String(note));
+		else if (note === null)
+			input.val("?");
+		else
+			input.val("");
+	});
+}
+
+function saveNotentabelle() {
+	for (var i in noten)
+		noten[i] = [];
+
+	$("#noten input").each(function () {
+		var input = $(this);
+		var note = input.val();
+		if (note.length) {
+			note = parseInt(note);
+			if (note === NaN)
+				noten[input.data("halbjahr")][
+					input.data("fach")] = null;
+			else
+				noten[input.data("halbjahr")][
+					input.data("fach")] = note;
+		}
+	});
+}
+
 $(function() {
 	initNotentabelle();
+	loadNotentabelle();
 });
 
